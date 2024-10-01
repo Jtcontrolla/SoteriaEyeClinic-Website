330def406ee42746d0671831aef108987a045593
Otonbara <alfredokolai@gmail.com>
2024-10-01 15:00:51 +0100

    js update

--- Soteria.js
+++ Soteria.js
@@ -1,11 +1,12 @@
 const toggleBtn = document.querySelector('.toggle_btn');
-const toggleBtnIcon = document.querySelector('.toggle_btn img');
+const toggleBtnIcon = document.querySelector('.toggle_btn i');
 const dropDownMenu = document.querySelector('.dropdown_menu');
 
 toggleBtn.onclick = function (){
     dropDownMenu.classList.toggle('open')
+    const isOpen = dropDownMenu.classList.contains('open')
+    
+    toggleBtnIcon.classList = isOpen
+        ? 'fa fa-times'
+        : 'fa fa-bars'
 }
-
-$(document).ready(function(){
-    $('.carousel').carousel();
-});
