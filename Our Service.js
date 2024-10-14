9b522323f55b02fc809d9dedee8eada24200e809
MrGhostX <mrghostx94@gmail.com>
2024-10-14 13:29:34 +0100

    update

---
+++ Our Service.js
@@ -0,0 +1,25 @@
+// Get the modal element
+var modal = document.getElementById("cataract-surgery-modal");
+
+// Get the button that opens the modal
+var btn = document.querySelector(".health-btn[data-target='#cataract-surgery-modal']");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close-btn")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
