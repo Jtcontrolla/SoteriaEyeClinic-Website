2d099d531bfd39f9922d3e8b1f4efe8460695cc1
Otonbara <alfredokolai@gmail.com>
2024-10-23 17:24:52 +0100

    swiper to the index

--- index.js
+++ index.js
@@ -10,3 +10,21 @@ toggleBtn.onclick = function (){
         ? 'fa fa-times'
         : 'fa fa-bars'
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper('.swiper-container', {
+      loop: true,
+      slidesPerView: 5,  // Adjust as needed
+      spaceBetween: 10,  // Space between slides
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      effect: 'slide',
+      speed: 600,
+      autoplay: {
+        delay: 3000,  // Adjust autoplay delay as needed
+        disableOnInteraction: false,
+      },
+    });
+});
